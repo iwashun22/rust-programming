cf1c041d04a5c84e42872386defc253886eb2e21
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-22 20:07:34 +0700

    update

---
+++ src/loops.rs
@@ -0,0 +1,47 @@
+pub fn run() {
+   let mut count = 0;
+
+   // Infinite Loop
+   // loop {
+   //    count += 1;
+   //    println!("Number: {}", count);
+      
+   //    if count == 20 {
+   //       break;
+   //    }
+   // }
+
+   // While Loop
+   while count <= 100 {
+      if count % 15 == 0 {
+         println!("FizzBuzz");
+      }
+      else if count % 3 == 0 {
+         println!("Fizz");
+      }
+      else if count % 5 == 0 {
+         println!("Buzz");
+      }
+      else {
+         println!("{}", count);
+      }
+
+      count += 1;
+   }
+
+   // For Range
+   for x in 0..100 {
+      if x % 15 == 0 {
+         println!("FizzBuzz");
+      }
+      else if x % 3 == 0 {
+         println!("Fizz");
+      }
+      else if x % 5 == 0 {
+         println!("Buzz");
+      }
+      else {
+         println!("{}", x);
+      }
+   }
+}
