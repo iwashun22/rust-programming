657e98a2a4464aac4f5be4c7d0ed5fca309da4d9
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-22 19:48:53 +0700

    update

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,9 +1,12 @@
 pub fn run() {
-   let age: u8 = 16;
-   
-   if age >= 18 {
-      println!("EMPLOYER: What role you wanna be?");
-   } else {
+   let age: u8 = 18;
+   let have_license: bool = true;
+
+   if age >= 18 && have_license {
+      println!("EMPLOYER: What role you looking for?");
+   } else if age < 18 {
       println!("EMPLOYER: Sorry, you are under the age.");
+   } else {
+      println!("EMPLOYER: Sorry, you need to get the license.");
    }
 }
