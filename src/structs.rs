83674dad7a29cd152bbbbceaeafabea1541259bb
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-26 21:48:48 +0700

    update

--- src/structs.rs
+++ src/structs.rs
@@ -7,6 +7,20 @@ struct Color {
    blue: u8
 }
 
+// Tuple Struct
+struct TupleColor(u8, u8, u8);
+
 pub fn run() {
-   println!("{}", std::u8::MAX);
+   let mut c = Color {
+      red: 255,
+      green: 0,
+      blue: 0
+   };
+   c.blue = 120;
+
+   println!("Color: RGB({}, {}, {})", c.red, c.green, c.blue);
+
+   let mut tc = TupleColor(0, 0, 255);
+   tc.1 = 80;
+   println!("Color: RGB({}, {}, {})", tc.0, tc.1, tc.2);
 }
