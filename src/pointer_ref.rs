4702380a07ff347ccd42ed3a200531c1824fe4c8
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-26 21:37:41 +0700

    update

--- src/pointer_ref.rs
+++ src/pointer_ref.rs
@@ -1,9 +1,14 @@
 // Reference Pointers - Point to a resource in memory
 
 pub fn run() {
-   // Primitive Array;
+   // Primitive - Array;
    let arr1 = [1, 2, 3];
    let arr2 = arr1;
 
-   println!("Values: {:?}", (arr1, arr2));
+   // Non-primitive - Vector
+   let vec1: Vec<i8> = vec![1, 2, 3];
+   let vec2: &Vec<i8> = &vec1;
+
+   println!("Values(Array): {:?}", (arr1, arr2));
+   println!("Values(Vector): {:?}", (&vec1, vec2));
 }
