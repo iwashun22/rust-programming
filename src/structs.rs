76dd63c1817f462d1e4cb878830bcb0e3e15b4b3
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-27 21:15:16 +0700

    update

--- src/structs.rs
+++ src/structs.rs
@@ -23,4 +23,50 @@ pub fn run() {
    let mut tc = TupleColor(0, 0, 255);
    tc.1 = 80;
    println!("Color: RGB({}, {}, {})", tc.0, tc.1, tc.2);
+
+   handle_person();
+}
+
+struct Person {
+   first_name: String, // You can't do &str
+   last_name: String,
+   age: u8
+}
+
+impl Person {
+   // Construct Person
+   fn new(first: &str, last: &str, age: u8) -> Person {
+      Person {
+         first_name: first.to_string(),
+         last_name: last.to_string(),
+         age: age
+      }
+   }
+   
+   // Get full name
+   fn full_name(&self) -> String {
+      format!("{} {}", self.first_name, self.last_name)
+   }
+
+   // Set last name
+   fn set_last_name(&mut self, last: &str) {
+      self.last_name = last.to_string();
+   }
+
+   // Get person to tuple
+   fn to_tuple(&self) -> (String, String, u8) {
+      (self.first_name.to_string(), self.last_name.to_string(), self.age)
+   }
+}
+
+fn handle_person() {
+   let mut p = Person::new("James", "Smith", 23);
+   // println!("Person: {} {}", p.first_name, p.last_name);
+   println!("Person: {}", p.full_name());
+   println!("Age: {}", p.age);
+
+   p.set_last_name("Williams");
+   println!("Person: {}", p.full_name());
+
+   println!("Person Tuple: {:?}", p.to_tuple());
 }
